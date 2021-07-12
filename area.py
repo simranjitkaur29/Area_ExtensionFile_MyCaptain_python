639d1b2fe1346a45ddf6bd9d128a117345b7df20
simranjit kaur <82168344+simranjitkaur29@users.noreply.github.com>
2021-07-12 18:02:09 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+#  write a Python program which accepts the radius of a circle from the user and computes area.
+Radius=float(input("Input the readius of the circle: "))
+area=3.14*Radius*Radius
+print("The area of the circle with radius ",Radius,"is:",area)
---
+++ extension.py
@@ -0,0 +1,6 @@
+#  write a Python program to accept a filename from the user and print the extension of that.
+a=open(input("Input the Filename:"),"a")
+c=input(" The extension of the file is : ")
+b=a.write(c)
+# print(b)
+a.close()
